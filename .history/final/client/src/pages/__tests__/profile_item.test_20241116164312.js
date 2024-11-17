32438e4997a10adc5926137a76dd5675c2559611
red-start25 <nakumdhruv@yahoo.com>
2024-11-16 19:06:07 -0800

    Profile adding trip and removing trip tested

---
+++ .history/final/client/src/pages/__tests__/profile_item.test_20241116164312.js
@@ -0,0 +1,82 @@
+// test/profileForm.test.js
+const { Builder, By, until } = require("selenium-webdriver");
+const assert = require("assert");
+
+const TIMEOUT = 2000;
+
+(async function profileItemTest() {
+  let driver = await new Builder().forBrowser("chrome").build();
+  await login(driver);
+  try {
+    // Navigate to the page containing the ProfileForm without .get 
+    await driver.get("http://localhost:3000/profile");
+
+    try {
+      await driver.wait(until.elementLocated(By.css("h2")), TIMEOUT);
+      // Check if the next page with the title "My Trips" with h2 is present
+      const title = await driver.findElement(By.css("h2")).getText();
+      assert.strictEqual(title, "My Trips");
+    } catch (error) {
+      throw new Error("Test Failed: Title not found");
+    }
+
+    // Assert that the success message is as expected
+    assert.strictEqual(true, true);
+
+    addItemToProfile(driver);
+
+    console.log("Test Passed: profile form submitted successfully.");
+  } catch (error) {
+    console.error("Test Failed:", error);
+  } finally {
+    await driver.quit();
+  }
+})();
+
+async function addItemToProfile(driver) {
+  await driver.get("http://localhost:3000/");
+ 
+  //find product with h3 name Starlink-15 (v1.0) and click
+  const product = await driver.findElement(By.css("h3"));
+  await product.click();
+
+  // find button with Test ADD TO CART and click
+  const addToCart = await driver.findElement(By.css("button"));
+  await addToCart.click();
+
+  await driver.get("http://localhost:3000/cart");
+  // find button BOOK ALL and click
+  const bookAll = await driver.findElement(By.css("button"));
+  await bookAll.click();
+
+  // goto profile page
+  await driver.get("http://localhost:3000/profile");
+
+  try {
+    await driver.wait(until.elementLocated(By.css("h2")), TIMEOUT);
+    // Check if the next page with the title "My Trips" with h2 is present
+    const title = await driver.findElement(By.css("h2")).getText();
+    assert.strictEqual(title, "My Trips");
+
+    // assert that the item was added to the profile page
+    const item = await driver.findElement(By.css("h3"));
+    assert.strictEqual(item, "Starlink-15 (v1.0)");
+
+    console.log("Test Passed: item added to profile.");
+  } catch (error) {
+    throw new Error("Test Failed: Title not found");
+}
+}
+
+async function login(driver) {
+  await driver.get("http://localhost:3000");
+  await driver.wait(until.elementLocated(By.css("form")), TIMEOUT);
+
+  const emailInput = await driver.findElement(By.css('input[name="email"]'));
+  await emailInput.sendKeys("test@gmail.com");
+
+  const submitButton = await driver.findElement(
+    By.css('button[type="submit"]')
+  );
+  await submitButton.click();
+}
---
+++ .history/final/client/src/pages/__tests__/profile_item.test_20241116190502.js
@@ -0,0 +1,124 @@
+// test/profileForm.test.js
+const { Builder, By, until } = require("selenium-webdriver");
+const assert = require("assert");
+
+const TIMEOUT = 1000;
+
+(async function profileItemTest() {
+  let driver = await new Builder().forBrowser("chrome").build();
+  await login(driver);
+  await addItemToProfile(driver);
+  await cancelTrip(driver);
+
+})();
+
+async function addItemToProfile(driver) {
+  await driver.get("http://localhost:3000/launch/109");
+  await driver.sleep(TIMEOUT);
+
+
+  // find button with Test ADD TO CART and click
+  const addToCart = await driver.findElement(By.css("button"));
+  await addToCart.click();
+await driver.sleep(TIMEOUT);
+
+
+  console.log("Test Passed: item added to cart.");
+
+  
+    // Wait for the "Cart" link to be visible
+    const cartLink = await driver.wait(
+        until.elementLocated(By.xpath("//a[@href='/cart']")),
+        TIMEOUT
+      );
+  
+      // Click the "Cart" link
+      await cartLink.click();
+  //add delay to let the page load
+  await driver.sleep(TIMEOUT);
+
+  // find button BOOK ALL and click
+  const bookAll = await driver.findElement(By.css("button"));
+  await bookAll.click();
+
+  await driver.sleep(TIMEOUT);
+
+
+  // goto profile page
+ const profileLink = await driver.wait(
+    until.elementLocated(By.xpath("//a[@href='/profile']")),
+    TIMEOUT
+  );
+  
+  await profileLink.click();
+  await driver.sleep(TIMEOUT);
+
+
+  try {
+    await driver.wait(until.elementLocated(By.css("h2")), TIMEOUT);
+    // Check if the next page with the title "My Trips" with h2 is present
+    const title = await driver.findElement(By.css("h2")).getText();  
+    assert.strictEqual(title, "My Trips");
+
+    // Wait for the "Starlink-15 (v1.0)" text to appear
+    const starlinkText = await driver.wait(
+        until.elementLocated(By.xpath("//h3[text()='Starlink-15 (v1.0)']")),
+        TIMEOUT
+      );
+  
+      // Get and print the text content
+      const textContent = await starlinkText.getText();
+      console.log('Found Item: ',textContent);
+
+    console.log("Test Passed: item added to profile.");
+  } catch (error) {
+    throw new Error("Test Failed: Title not found");
+}
+}
+
+async function login(driver) {
+  await driver.get("http://localhost:3000");
+  await driver.wait(until.elementLocated(By.css("form")), TIMEOUT);
+
+  const emailInput = await driver.findElement(By.css('input[name="email"]'));
+  await emailInput.sendKeys("test@gmail.com");
+
+  const submitButton = await driver.findElement(
+    By.css('button[type="submit"]')
+  );
+  await submitButton.click();
+}
+
+async function cancelTrip(driver){
+
+  await driver.sleep(TIMEOUT);
+
+  // Wait for the element with the "Starlink-15 (v1.0)" text to appear
+  const starlinkElement = await driver.wait(
+    until.elementLocated(By.xpath("//h3[text()='Starlink-15 (v1.0)']")),
+    TIMEOUT // Timeout in milliseconds
+  );
+  
+  await starlinkElement.click();
+
+   const cancelTrip = await driver.findElement(By.css("button"));
+   await cancelTrip.click();
+ 
+   await driver.sleep(TIMEOUT);
+
+  const profile = await driver.wait(
+    until.elementLocated(By.xpath("//a[@href='/profile']")),
+    TIMEOUT
+  );
+
+  await profile.click();
+
+  try {
+    const noTrip = await driver.findElement(By.css("p")).getText();
+    assert.strictEqual(noTrip, "You haven't booked any trips");
+    console.log("Test Passed: Cancel Trip Functionality Working.");
+  } catch (error) {
+    throw new Error("Test Failed: Title not found");
+}
+
+}
